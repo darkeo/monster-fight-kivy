678b889d28ab19e110fd0e5ef2543bd152aaab28
Léo Leclerc <leoleclerc905@gmail.com>
2022-04-24 02:15:37 +0200

    V0.3 ajout en chantier de barre XP + barre de vie et ajout des sons

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition, NoTransition, CardTransition, SwapTransition, \
     FadeTransition, WipeTransition, FallOutTransition, RiseInTransition
 from kivy.properties import StringProperty, NumericProperty, ObjectProperty
+from kivy.core.audio import SoundLoader
 
 
 class MainWidget(BoxLayout):
@@ -20,8 +21,47 @@ class AllScreen(Screen):
     Ennemy_img = StringProperty("images/slime.png")
     Effect_img = StringProperty("images/void.png")
 
-    def on_spell_pressed(self):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.init_audio()
+
+    def on_attack(self):
+        self.sword_sound.play()
+        self.ids.Effect_spell.source = "images/slash.png"
+
+    def spell_effect_reset(self):
+        self.ids.Effect_spell.source = "images/void.png"
+
+    def on_spell_fire_pressed(self):
+        self.fire_sound.play()
         self.ids.Effect_spell.source = "images/fireball.png"
+
+    def on_spell_wind_pressed(self):
+        self.wind_sound.play()
+        self.ids.Effect_spell.source = "images/tornado.png"
+
+    def on_spell_heal_pressed(self):
+        self.heal_sound.play()
+        self.ids.Effect_spell.source = "images/heal.png"
+
+    def ennemy_killed(self):
+        self.ids.Ennemy_img.source = "images/void.png"
+
+    sword_sound = None
+    fire_sound = None
+    wind_sound = None
+    heal_sound = None
+
+    def init_audio(self):
+        self.sword_sound = SoundLoader.load("audio/sword.wav")
+        self.fire_sound = SoundLoader.load("audio/fireball.wav")
+        self.wind_sound = SoundLoader.load("audio/tornado.wav")
+        self.heal_sound = SoundLoader.load("audio/healing.wav")
+
+        self.sword_sound.volume = 5
+        self.fire_sound.volume = 3
+        self.wind_sound.volume = 3
+        self.heal_sound.volume = 5
 
 
 class MenuScreen(AllScreen):
