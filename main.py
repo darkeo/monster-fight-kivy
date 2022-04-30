63a69613d1e9c9203727a4062e9d38e4cc70c790
Léo Leclerc <leoleclerc905@gmail.com>
2022-04-30 02:25:51 +0200

    V0.5 ajout de l'écran game over et transition avec le menu + reset des stats, ajout du mana V1

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition, NoTra
 from kivy.properties import StringProperty, NumericProperty, ObjectProperty
 from kivy.core.audio import SoundLoader
 
+game_won = False
+game_over = False
+
 
 class MainWidget(BoxLayout):
     Ennemy_img = StringProperty("images/slime.png")
@@ -23,8 +26,11 @@ class MainWidget(BoxLayout):
     monster_hp_left_ratio = NumericProperty(1)
     hero_hp_lost = NumericProperty(0)
     hero_hp = NumericProperty(100)
+    hero_mp_lost = NumericProperty(0)
+    hero_mp = NumericProperty(100)
     hp_left_ratio = NumericProperty(1)
 
+    fizzle_sound = None
     game_over_sound = None
     sword_sound = None
     fire_sound = None
@@ -37,6 +43,7 @@ class MainWidget(BoxLayout):
         self.init_audio()
 
     def init_audio(self):
+        self.fizzle_sound = SoundLoader.load("audio/Fizzle.wav")
         self.sword_sound = SoundLoader.load("audio/sword.wav")
         self.fire_sound = SoundLoader.load("audio/fireball.wav")
         self.wind_sound = SoundLoader.load("audio/tornado.wav")
@@ -51,33 +58,66 @@ class MainWidget(BoxLayout):
         self.victory_sound.volume = 1
         self.game_over_sound.volume = 5
 
+    def reset_all(self):
+        self.Ennemy_img = "images/slime.png"
+        self.Effect_img = "images/void.png"
+        self.ids.Effect_spell.source = "images/void.png"
+        self.menu_size = 1
+        self.menu_size_x = 1
+        self.monster_hp_lost = 0
+        self.monster_hp = 100
+        self.monster_hp_left_ratio = 1
+        self.hero_hp_lost = 0
+        self.hero_hp = 100
+        self.hp_left_ratio = 1
+
     def spell_effect_reset(self):
         self.ids.Effect_spell.source = "images/void.png"
 
     def monster_turn(self):
+        global game_won
+        global game_over
         if self.monster_hp_lost >= self.monster_hp:
             self.victory_sound.play()
+            game_won = True
+            self.reset_all()
         if self.hero_hp_lost >= self.hero_hp:
-            self.game_over_sound.play()
+            game_over = True
+            self.reset_all()
 
     def on_spell_fire_pressed(self):
-        self.fire_sound.play()
-        self.ids.Effect_spell.source = "images/fireball.png"
-        self.monster_hp_lost += 30
-        # self.monster_hp_left_ratio = (self.monster_hp - self.hp_lost) / self.monster_hp
-        self.monster_turn()
+        mp_cost = 35
+        if self.hero_mp - self.hero_mp_lost>= mp_cost:
+            self.fire_sound.play()
+            self.ids.Effect_spell.source = "images/fireball.png"
+            self.monster_hp_lost += 30
+            self.hero_mp_lost += 30
+            # self.monster_hp_left_ratio = (self.monster_hp - self.hp_lost) / self.monster_hp
+            self.monster_turn()
+        else:
+            self.fizzle_sound.play()
 
     def on_spell_wind_pressed(self):
-        self.wind_sound.play()
-        self.ids.Effect_spell.source = "images/tornado.png"
-        self.hero_hp_lost += 20
-        self.monster_turn()
+        mp_cost = 20
+        if self.hero_mp - self.hero_mp_lost >= mp_cost:
+            self.wind_sound.play()
+            self.ids.Effect_spell.source = "images/tornado.png"
+            self.hero_hp_lost += 20
+            self.hero_mp_lost += 20
+            self.monster_turn()
+        else:
+            self.fizzle_sound.play()
 
     def on_spell_heal_pressed(self):
-        self.heal_sound.play()
-        self.ids.Effect_spell.source = "images/heal.png"
-        self.hero_hp_lost -= 20
-        self.monster_turn()
+        mp_cost = 20
+        if self.hero_mp - self.hero_mp_lost >= mp_cost:
+            self.heal_sound.play()
+            self.ids.Effect_spell.source = "images/heal.png"
+            self.hero_hp_lost -= 20
+            self.hero_mp_lost += mp_cost
+            self.monster_turn()
+        else:
+            self.fizzle_sound.play()
 
 
 class AllScreen(Screen):
@@ -88,6 +128,9 @@ class AllScreen(Screen):
 
     def ennemy_killed(self):
         self.ids.Ennemy_img.source = "images/void.png"
+
+    def change_screen(self, screen_to_go):
+        self.manager.current = screen_to_go
 
 
 class MenuFull(TabbedPanel):
@@ -103,10 +146,44 @@ class MagicMenu(BoxLayout):
 
 
 class MenuScreen(AllScreen):
-    pass
+    def reset_game_won(self):
+        global game_won
+        global game_over
+
+        game_won = False
+        game_over = False
+
+
+class GameOverScreen(AllScreen):
+    game_over_sound = None
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.init_audio()
+
+    def init_audio(self):
+        self.game_over_sound = SoundLoader.load("audio/Game_over.wav")
+
+        self.game_over_sound.volume = 5
+
+    def reset_game_over(self):
+        global game_won
+        global game_over
+
+        game_won = False
+        game_over = False
 
 
 class FightScreen(AllScreen):
+
+    def check_game_end(self):
+        if game_won:
+            self.manager.current = 'menu'
+        elif game_over:
+            self.manager.current = 'game_over'
+
+
+class TryAgainButton(Button):
     pass
 
 
@@ -140,6 +217,7 @@ class MonsterFightApp(App):
         sm = ScreenManager(transition=NoTransition())
         sm.add_widget(MenuScreen(name='menu'))
         sm.add_widget(FightScreen(name='combat'))
+        sm.add_widget(GameOverScreen(name='game_over'))
 
         return sm
 
